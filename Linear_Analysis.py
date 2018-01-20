5227565d05c46b961e0209c7c1b512b4e3ec6c17
Carson Hanel <carsonh14@gmail.com>
2018-01-20 15:23:22 -0600

    Added more functions

--- Linear_Analysis.py
+++ Linear_Analysis.py
@@ -8,6 +8,7 @@ Note: These code snippets are derived from Data Science from Scratch First Princ
       the Python standard library or a package already created, I thought it would be useful to begin
       creating my own data science toolbelt for the future, with self written commentary.
 """
+import math
 
 class Linear_Analysis():
       """
@@ -39,10 +40,46 @@ class Linear_Analysis():
             create a sum across all vectors, all vectors must be added to some vector; namely 
             the 0'th. From then, the function iterates from the 1'st vector onward, adding all
             subsequent vectors to 0'th, and finally returning the accumulated sum.
+
+            Some useful application from the book:
+            
+                  def vector_sum(vectors):
+                        return reduce(vector_add, vectors)
+                        
+            Which is a higher order function that reduces the vector space to a singular vector
+            by utilizing the given function, "vector_add". It's not necessary, but is a good usage
+            of space if that's the type of best practice you're after.
       """
       def vector_sum (vectors):
             result = vectors[0]
             for vector in vectors[1:]:
                   result = vector_add(result, vector)
             return result
+
+      """
+      Scalar multiplication:
+            Used for multiplying a vector by a scalar. Essentially vector_add with multiplication,
+            and rather than a second vector being zipped and summed, all points are simply multiplied
+            by a constant.
+      """
+      def scalar_multiply(c, v):
+            return [c * v_i for v_i in v]
+
+      """
+      Dot product:
+            Used for multiplying the pointwise information of two vectors into pointwise products.
+            Simply works like vector_sum, but rather than the sum, you're deriving the product of
+            the two vectors; a simple algorithm.
+      """
+      def dot(v, w):
+            return sum(v_i * w_i for v_i, w_i in zip(v,w))
+
+      ""
+      Sum of squares:
+            The multiplication of a vector by itself. Utilized for determining the dispersion of a 
+            vector which is representative of a set of data. Also utilized in determining the line
+            of best fit for a dataset.
+      """
+      def sum_of_squares(v):
+            return dot(v, v)
                   
