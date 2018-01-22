f1b4c5fecc2653fdba0b7f69312e4745f3d68aef
Carson Hanel <carsonh14@gmail.com>
2018-01-22 14:00:22 -0600

    Added histogram, distribution comparison

--- 3. Probability_Analysis.py
+++ 3. Probability_Analysis.py
@@ -37,6 +37,54 @@ class Probability_Analysis:
       def normal_cdf(x, mu=0, sigma=1):
             return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
       
+      """
+      Inverse Normal Cumulative Function:
+            Essentially a binary search looking for the correct ICDF given the tolerance.
+      """
+      def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+            if mu != 0 or sigma != 1:
+                  return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+            
+            low_z, low_p = -10.0, 0
+            hi_z,  hi_p  =  10.0, 1
+            while hi_z - low_z > tolerance:
+                  mid_z = (low_z + hi_z) / 2
+                  mid_p = normal_cdf(mid_z)
+                  if mid_p < p:
+                        low_z, low_p = mid_z, mid_p
+                  elif mid_p > p:
+                        hi_z, hi_p = mid_z, mid_p
+                  else:
+                        break
+                        
+            return mid_z
+      
+      def bernoulli_trial(p):
+            return 1 if random.random() < p else 0
+      
+      def binomial(n, p):
+            return sum(bernoulli_trial(p) for _ in range(n))
+      
+      def make_hist(p, n, num_points):
+            data = [binomial(n, p) for _ in range(num_points)]
+            
+            # use a bar chart to show the actual binomial samples
+            histogram = Counter(data)
+            plt.bar([x - 0.4 for x in histogram.keys()],
+                    [v / num_points for v in histogram.values()],
+                    0.8,
+                    color='0.75')
+            
+            mu    = p * n 
+            sigma = math.sqrt(n * p * (1 - p))
+            
+            # use a line chart to show the normal approximation
+            xs = range(min(data), max(data) + 1)
+            ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma) for i in xs]
+            plt.plot(xs, ys)
+            plt.title("Binomial Distribution vs. Normal Approximation")
+            plt.show()
+             
 ###########################################################################
 # Given experiment from the textbook, making example of random variables: #
 ###########################################################################
