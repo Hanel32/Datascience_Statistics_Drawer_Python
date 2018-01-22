d808d65dcdf721ee5caee879a177d8acad41ce45
Carson Hanel <carsonh14@gmail.com>
2018-01-22 13:47:22 -0600

    Added Normal CDF

--- 3. Probability_Analysis.py
+++ 3. Probability_Analysis.py
@@ -31,7 +31,11 @@ class Probability_Analysis:
             sqrt_two_pi = math.sqrt(2 * math.pi)
             return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
       
-      
+      """
+      Normal Cumulative Distribution Function:
+      """
+      def normal_cdf(x, mu=0, sigma=1):
+            return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
       
 ###########################################################################
 # Given experiment from the textbook, making example of random variables: #
@@ -59,11 +63,22 @@ print "P(both | either):", both_girls / either_girl
 # Examples of Normal Probability Density Functions:                       #
 ###########################################################################
 xs = [x / 10.0 for x in range(-50, 50)]
-plt.plot(xs, [normal_pdf(x, sigma=1)  for x in xs], '-', label = 'mu=0,  sigma=1')
-plt.plot(xs, [normal_pdf(x, sigma=2)  for x in xs], '-', label = 'mu=0,  sigma=2')
-plt.plot(xs, [normal_pdf(x, sigma0.5) for x in xs], '-', label = 'mu=0,  sigma=.5')
-plt.plot(xs, [normal_pdf(x, mu=-1)    for x in xs], '-', label = 'mu=-1, sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=1)  for x in xs], '-' , label = 'mu=0,  sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=2)  for x in xs], '--', label = 'mu=0,  sigma=2')
+plt.plot(xs, [normal_pdf(x, sigma0.5) for x in xs], ':' , label = 'mu=0,  sigma=.5')
+plt.plot(xs, [normal_pdf(x, mu=-1)    for x in xs], '-.', label = 'mu=-1, sigma=1')
 plt.legend()
 plt.title("Various Normal pdfs")
 plt.show()
               
+###########################################################################
+# Examples of Normal Cumulative Distribution Functions:                   #
+###########################################################################
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_cdf(x, sigma=1)  for x in xs], '-' , label = 'mu=0,  sigma=1')
+plt.plot(xs, [normal_cdf(x, sigma=2)  for x in xs], '--', label = 'mu=0,  sigma=2')
+plt.plot(xs, [normal_cdf(x, sigma0.5) for x in xs], ':' , label = 'mu=0,  sigma=.5')
+plt.plot(xs, [normal_cdf(x, mu=-1)    for x in xs], '-.', label = 'mu=-1, sigma=1')
+plt.legend(loc=4)
+plt.title("Various Normal cdfs")
+plt.show()
