68256b7ee80069ae483ae01f61f3925fc352977a
Carson Hanel <carsonh14@gmail.com>
2018-01-22 13:31:02 -0600

    Included normal PDF example

--- 3. Probability_Analysis.py
+++ 3. Probability_Analysis.py
@@ -7,3 +7,63 @@ Note: These code snippets are derived from Data Science from Scratch First Princ
       creating my own data science toolbelt for the future, with self written commentary.
 """
 class Probability_Analysis:
+      """
+      Uniform probabiliy density function:
+      
+      """
+      def uniform_pdf(x):
+            return 1 if x >= 0 and x < 1 else 0
+      
+      """
+      Uniform cumulative distribution function:
+      
+      """
+      def uniform_cdf(x):
+            if x < 0:   return 0
+            elif x < 1: return x
+            else:       return 1
+           
+      """
+      Normal Probability Density Function:
+      
+      """
+      def normal_pdf(x, mu = 0, sigma = 1):
+            sqrt_two_pi = math.sqrt(2 * math.pi)
+            return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+      
+      
+      
+###########################################################################
+# Given experiment from the textbook, making example of random variables: #
+###########################################################################
+def random_kid():
+      return random.choice(["boy", "girl"])
+both_girls  = 0
+older_girl  = 0
+either_girl = 0
+
+random.seed(0)
+for _ in range(10000):
+      younger = random_kid()
+      older   = random_kid()
+      if older == "girl":
+            older_girl  += 1
+      if older == "girl" and younger == "girl":
+            both_girls  += 1
+      if older == "girl" or  younger == "girl":
+            either_girl += 1
+print "P(both | older):", both_girls / older_girl
+print "P(both | either):", both_girls / either_girl
+
+###########################################################################
+# Examples of Normal Probability Density Functions:                       #
+###########################################################################
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_pdf(x, sigma=1)  for x in xs], '-', label = 'mu=0,  sigma=1')
+plt.plot(xs, [normal_pdf(x, sigma=2)  for x in xs], '-', label = 'mu=0,  sigma=2')
+plt.plot(xs, [normal_pdf(x, sigma0.5) for x in xs], '-', label = 'mu=0,  sigma=.5')
+plt.plot(xs, [normal_pdf(x, mu=-1)    for x in xs], '-', label = 'mu=-1, sigma=1')
+plt.legend()
+plt.title("Various Normal pdfs")
+plt.show()
+              
