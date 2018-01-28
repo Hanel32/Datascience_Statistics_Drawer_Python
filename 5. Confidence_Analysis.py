af0f6eaf572801507ed585c0644ca03ac74dd201
Carson Hanel <carsonh14@gmail.com>
2018-01-28 00:20:56 -0600

    Initial commit
    Header input

---
+++ 5. Confidence_Analysis.py
@@ -0,0 +1,8 @@
+"""
+@author: Carson Hanel
+Note: These code snippets are derived from Data Science from Scratch First Principles by Joel Grus.
+      For right now, I'll be transferring the code from the book, explaining the functions, and creating
+      an API that can be utilized in further analysis. While some of these functions may be part of 
+      the Python standard library or a package already created, I thought it would be useful to begin
+      creating my own data science toolbelt for the future, with self written commentary.
+"""
